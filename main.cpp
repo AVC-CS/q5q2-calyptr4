1a2339c9af9951c9719b2a232c1fa22f81b833ec
calyptr4 <alessandrabaires@gmail.com>
2026-04-13 13:52:52 -0700

    q2

--- main.cpp
+++ main.cpp
@@ -3,16 +3,24 @@ using namespace std;
 
 int main()
 {
-    int N, M;
+    int N, M, result=1;
 
     cout << "Enter your base number: ";
     cin >> N;
     cout << "Enter your exponent: ";
     cin >> M;
 
-    cout << N << " to power number: ";
+    if (N<=0 || N>=10 || M<1 || M>10){
+        cout << "Invalid base number" << endl;
+        return 1;
+    }
 
-    // TODO
+    cout << N << " to power number: " << result << " ";
+
+    for (int i=0; i<M; i++){
+        result *= N;
+        cout << result << " ";
+    }
 
     cout << endl;
 
